456d8fc3c54bee9b5537845b7a12c8e0a10c8102
Amari Ashwood <fromashestosnow@gmail.com>
2016-08-02 14:00:34 -0500

    added files and basic tests

---
+++ birdyboard_tests.py
@@ -0,0 +1,30 @@
+import unittest
+
+# from birdyboard_menu import *
+# from birdyboard_new_user import *
+# from birdyboard_view_chirps import *
+# from birdyboard_public_chirp import *
+# from birdyboard_private_chirp import *
+
+class TestBirdyBoard(unittest.TestCase):
+
+  @classmethod
+  def firstTestForTestReasons(self): #pass in self. that's important
+    #something goes here
+    pass
+
+  def test_menu(self):
+    pass
+
+  def test_create_user(self):
+    pass
+
+  def test_view_chirps(self):
+    pass
+
+  def test_public_chirp(self):
+    pass
+
+  def test_private_chirp(self):
+    pass
+
